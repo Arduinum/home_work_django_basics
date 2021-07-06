c08683815e8a6c29edc9192db392512e174406fd
Arduinum <message.chaos628@gmail.com>
2021-07-06 12:51:10 +0300

    added error_fill_db

---
+++ geekshop/mainapp/managment/commands/fill_db.py
@@ -0,0 +1,39 @@
+import os
+import json
+
+from django.contrib.auth.models import User # модель чтоб создать user
+from django.core.management.base import BaseCommand # класс который создаёт механику команд
+
+from authapp.models import ShopUser
+from mainapp.models import ProductCategory, Product
+
+JSON_PATH = 'mainapp/jsons' # путь до json
+
+# функция для чтения json
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), mode='r', encoding='UTF-8') as infile:
+
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categories = load_from_json('categories')
+
+        ProductCategory.ojects.all().delete() # почистили нашу модель
+        for category in categories:
+            new_category = ProductCategory(**category) # ** - распаковка
+            new_category.save() # сораняем изменения в таблице
+        
+        products = load_from_json('products')
+
+        Product.objects.all().delete()
+        for product in products:
+            category_name = product['category']
+            _category = ProductCategory.ojects.get(name=category_name)
+            product['category'] = _category
+            new_category = Product(**product)
+            new_category.save()
+        
+        # создаём пользователя
+        super_user = ShopUser.objects.create_superuser('admin', 'admin@geekshop.local', '123', age='30')
