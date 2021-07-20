78a8b8d327349ee6d22d22e5bec97d1455b8bbe2
Arduinum <message.chaos628@gmail.com>
2021-07-20 19:34:48 +0300

    added home work 8

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -4,7 +4,7 @@ from basketapp.models import Basket
 
 def index(request):  # request - словарь прилетающий на сервак
     title = 'магазин'
-    products = Product.objects.all()[:5]
+    products = Product.objects.all()[:4]
     
     basket = []
     if request.user.is_authenticated:
@@ -20,8 +20,4 @@ def index(request):  # request - словарь прилетающий на се
 
 
 def contacts(request):
-    title = 'контакты'
-    context = {
-        'title': title
-    }
-    return render(request=request, template_name='geekshop/contact.html', context=context)
+    return render(request=request, template_name='geekshop/contact.html')
