ec759e54ece21b2583b770cdf5bf94f1c7f95a16
Kaddo <message.chaos628@gmail.com>
2021-07-27 10:31:03 +0300

    updated homework 7 and 8

--- geekshop/adminapp/urls.py
+++ geekshop/adminapp/urls.py
@@ -1,19 +1,21 @@
 from django.urls import path
 import adminapp.views as adminapp
-from adminapp.views import UsersListView, UserCreateView
+from adminapp.views import UsersListView, UserCreateView, ProductCategoryCreateView, ProductCategoryListView
 
 app_name = 'adminapp' # обязательно с namespace
 
 urlpatterns = [
-    # path('users/create/', adminapp.user_create, name='user_create'),
+    # path('users/create/', adminapp.user_create, name='user_create'), - на функциях если
     path('users/create/', UserCreateView.as_view(), name='user_create'),
     # path('users/read/', adminapp.users, name='users'),
     path('users/read/', UsersListView.as_view(), name='users'),
     path('users/update/<int:pk>/', adminapp.user_update, name='user_update'),
     path('users/delete/<int:pk>/', adminapp.user_delete, name='user_delete'),
 
-    path('categories/create/', adminapp.category_create, name='category_create'),
-    path('categories/read/', adminapp.categories, name='categories'),
+    # path('categories/create/', adminapp.category_create, name='category_create'),
+    path('categories/create/', ProductCategoryCreateView.as_view(), name='category_create'),
+    # path('categories/read/', adminapp.categories, name='categories'),
+    path('categories/read/', ProductCategoryListView.as_view(), name='categories'),
     path('categories/update/<int:pk>/', adminapp.category_update, name='category_update'),
     path('categories/delete/<int:pk>/', adminapp.category_delete, name='category_delete'),
 
